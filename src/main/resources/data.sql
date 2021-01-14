9b70259881ba8c75a9db1551217cf428983afbb5
Babos Csaba <babos.csaba@yourweb.hu>
2021-01-14 23:02:21 +0100

    Initial state

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO todos (task_description) VALUES
+    ("First ever task in the database"),
+    ("This should work and I'll be happy"),
+    ("Should sleep earlier than usual");
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE todos (
+   id INT AUTO_INCREMENT PRIMARY KEY,
+   task_description VARCHAR(255),
+   created DATETIME,
+   completed NUMBER(1)
+);
